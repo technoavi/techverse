272ab1be2383edd44a9ab80346c430d1fc3ce813
naren1996 <narendrac143@gmail.com>
2023-08-08 01:22:28 +0530

    Dockerfile added

--- Narender/customer-service/src/main/java/com/narensoft/customerservice/CustomerServiceApplication.java
+++ Narender/customer-service/src/main/java/com/narensoft/customerservice/CustomerServiceApplication.java
@@ -3,13 +3,20 @@ package com.narensoft.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	@GetMapping("/msg")
+	public String message(){
+		return "Hello World";
+	}
 }
