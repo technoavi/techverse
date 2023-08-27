3bb3cd0a661d83a3ab446443e359546ea3fc5056
Avinash Srivastava <avs0949@gmail.com>
2023-08-27 23:16:47 +0530

    adding circuit breaker with retry mechanism

--- fz.circuit-breaker/src/main/java/com/technoavi/fz/cb/controller/FCControler.java
+++ fz.circuit-breaker/src/main/java/com/technoavi/fz/cb/controller/FCControler.java
@@ -1,11 +1,13 @@
 package com.technoavi.fz.cb.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,13 +20,14 @@ public class FCControler {
     private RestTemplate restTemplate;
 
     @GetMapping("/gt")
-    @CircuitBreaker(name="nm", fallbackMethod = "callMSG_fallback")
+    @CircuitBreaker(name="myCircuitBreaker", fallbackMethod = "callMSG_fallback")
+    @Retry(name = "myRetry")
     public String getMSGfromCS(){
         log.info("Activity received: getMSGfromCS" );
         String uri="http://localhost:8082/msg";
-        ResponseEntity<String> res= restTemplate.getForEntity(uri, String.class);
-        log.info("Activity received: getMSGfromCS" +uri +" and "+res.getBody());
-       return "from Customer service -"+res.getBody();
+        String res= restTemplate.getForObject(uri, String.class);
+        log.info("Activity received: getMSGfromCS" +uri +" and "+res);
+       return "from Customer service -"+res;
     }
 
 
@@ -36,7 +39,7 @@ public class FCControler {
         return "from Fisherman service";
     }
 
-    private  String callMSG_fallback(Throwable th){
+    private  String callMSG_fallback(Exception ex){
 
         return "plz try again!!";
     }
--- fz.circuit-breaker/src/main/java/com/technoavi/fz/cb/exception/CircuitException.java
+++ fz.circuit-breaker/src/main/java/com/technoavi/fz/cb/exception/CircuitException.java
@@ -1,7 +1,7 @@
-package com.technoavi.fz.cb;
+package com.technoavi.fz.cb.exception;
 
 public class CircuitException extends Exception{
 
 
-  
+
 }
